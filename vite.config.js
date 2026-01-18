26b5457463c72c9fce80d42992c44f227d0dbc99
randhirdinesh <117707091+randhirdinesh@users.noreply.github.com>
2026-01-19 02:26:50 +0400

    Fix quotes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Randhir_portfolio/',   // must match repo name exactly
-})
+  base: "/Randhir_portfolio/",
+});
